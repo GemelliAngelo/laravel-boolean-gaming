81bc32b6d997852ace01cfe91898c4b95db2441b
GemelliAngelo <gemeangelo27@gmail.com>
2025-04-14 12:50:39 +0200

    index + layouts + components

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Boolean Gaming</title>
-    @vite(['resources/sass/app.scss','resources/js/app.js'])
-</head>
-<body>
-    <div class="container">
-        <h1>Hello world</h1>
-    </div>
-</body>
-</html>
+@extends('layouts.games')
+@section("title","Hello world")
+
+@section('content')
+    <h2>Sei nella index</h2>
+@endsection

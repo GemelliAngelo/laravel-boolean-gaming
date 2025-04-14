a861dae4d9dac3c3b75e608a53157c420becf7b2
GemelliAngelo <gemeangelo27@gmail.com>
2025-04-14 13:42:01 +0200

    add homepage content

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.games')
-@section("title","Hello world")
+@section("title","Benvenuto nel pannello di amministrazione")
 
 @section('content')
-    <h2>Sei nella index</h2>
+<div class="card text-center">
+    <div class="card-header">
+      Pannello di Accesso
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">ACCEDI PER CONTINUARE</h5>
+      <p class="card-text">Una volta effettuato l'accesso puoi gestire tutti i contenuti della tua area di riferimento</p>
+      <a href="#" class="btn btn-primary">ACCEDI</a>
+    </div>
+</div>
 @endsection

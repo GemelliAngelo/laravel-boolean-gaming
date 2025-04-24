864bc876c09c0334453ace2764e239eb04f39417
GemelliAngelo <gemeangelo27@gmail.com>
2025-04-24 13:57:27 +0200

    add json response

--- app/Http/Controllers/Api/GamesController.php
+++ app/Http/Controllers/Api/GamesController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Game;
 use Illuminate\Http\Request;
 
 class GamesController extends Controller
 {
     public function index()
     {
-        return 'sei nella index';
+
+        $games = Game::with('platforms')->get();
+
+        return response()->json(
+            [
+                'success' => true,
+                'data' => $games,
+            ]
+        );
     }
 }
